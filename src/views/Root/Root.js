c5dad34163eadb17ce9d8092f67f0549b7319264
Dominika <fud.dominika@gmail.com>
2023-03-17 12:20:45 +0100

    routes setup

--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import Button from 'components/atoms/Button/Button';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import 'styles/base/_reset.scss';
 
+import NotesView from 'views/NotesView/NotesView';
+import ArticlesView from 'views/ArticlesView/ArticlesView';
+import TwittersView from 'views/TwittersView/TwittersView';
+
 const Root = () => (
-  <div>
-    <Button>Save</Button>
-    <Button secondary>Remove</Button>
-  </div>
+  <BrowserRouter>
+    <Routes>
+      <Route exact path="/" element={<NotesView />} />
+      <Route path="/articles" element={<ArticlesView />} />
+      <Route path="/twitters" element={<TwittersView />} />
+    </Routes>
+  </BrowserRouter>
 );
 
 export default Root;
